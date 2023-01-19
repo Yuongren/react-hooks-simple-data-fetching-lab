242f190721a3d83a5f8dd6ef089a755647d7f6b4
gitYoungren <youngrengitonga@gmail.com>
2023-01-19 09:45:15 +0300

    completed assignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom/extend-expect";
 import { server } from "../mocks/server";
 
 import App from "../components/App";
-
+  
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+
+function App() {
+    const [picture, setPicture] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() =>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setPicture(data);
+            setLoading(true);
+        });
+    }, []);
+
+    if (!loading) return <p>loading ...</p>;
+    return (
+        <div className="App">
+          <img src={picture.message} alt="A Random Dog" />
+        </div>
+    )
+}
+
+export default App
